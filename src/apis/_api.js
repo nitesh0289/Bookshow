bd86e8d3f8542c16ee5d4baa6057e68126d9f7ff
nitesh0289 <niteshch200053@gmail.com>
2025-07-24 14:53:51 +0530

    updated the backend basurl

--- src/apis/_api.js
+++ src/apis/_api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "https://bookshow-j73m.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   },
